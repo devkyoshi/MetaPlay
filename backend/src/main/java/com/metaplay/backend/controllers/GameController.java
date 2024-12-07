84a8400e1aacbc7b63b5b9d72026905f6cb3bd37
Ashan Tharindu <153891101+DevAshiZ@users.noreply.github.com>
2024-12-07 11:03:48 +0530

    update method

--- backend/src/main/java/com/metaplay/backend/controllers/GameController.java
+++ backend/src/main/java/com/metaplay/backend/controllers/GameController.java
@@ -5,10 +5,7 @@ import com.metaplay.backend.dto.GameRequestDTO;
 import com.metaplay.backend.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/games")
@@ -21,6 +18,15 @@ public class GameController {
     public ResponseEntity<Boolean> addGame(@RequestBody GameRequestDTO gameRequestDTO) {
         boolean isSaved = gameService.addNewGame(gameRequestDTO);
         if(!isSaved) {
+            return ResponseEntity.badRequest().body(false);
+        }
+        return ResponseEntity.ok(true);
+    }
+
+    @PostMapping("/update/{gameId}")
+    public ResponseEntity<Boolean> updateGame(@RequestBody GameRequestDTO gameRequestDTO, @PathVariable Long gameId) {
+        boolean isUpdated = gameService.updateGame(gameId, gameRequestDTO);
+        if(!isUpdated) {
             return ResponseEntity.badRequest().body(false);
         }
         return ResponseEntity.ok(true);
--- backend/src/main/java/com/metaplay/backend/services/GameService.java
+++ backend/src/main/java/com/metaplay/backend/services/GameService.java
@@ -30,4 +30,35 @@ public class GameService {
             return false;
         }
     }
+
+    public boolean updateGame(Long gameId, GameRequestDTO gameRequestDTO) {
+        try{
+            // Find the game by id
+            Game game = gameRepository.findById(gameId).orElse(null);
+
+            if(game == null) {
+                log.error("Game not found with id: {}", gameId);
+                return false;
+            }
+
+            // Update the game
+            if(gameRequestDTO.getTitle() != null){
+                log.info("Updating game title to: {}", gameRequestDTO.getTitle());
+                game.setTitle(gameRequestDTO.getTitle());
+            }
+            if(gameRequestDTO.getCategory() != null){
+                log.info("Updating game category to: {}", gameRequestDTO.getCategory());
+                game.setCategory(gameRequestDTO.getCategory());
+            }
+
+            // Save the updated game to the database
+            gameRepository.save(game);
+            log.info("Game updated successfully with id: {}", gameId);
+            return true;
+        } catch (Exception e) {
+            log.error("Error while updating the game: {}", e.getMessage());
+           return false;
+        }
+
+    }
 }
